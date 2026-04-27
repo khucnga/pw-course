de6e2cc0843e670fe1d1e9210708937d8a5c7ea6
Nga Khuc <khucnga96@gmail.com>
2026-04-27 11:44:22 +0700

    homework

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,9 @@
+const number = 12;
+let name = "My number";
+let isEven = false;
+if (number % 2 === 0) { 
+    isEven = true;
+}
+console.log("number:", number);
+console.log("name:", name);
+console.log("isEven:", isEven); 
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,5 @@
+const myHeight = 162;
+const even = myHeight % 100;
+console.log("Cân nặng lý tưởng", even * 9 / 10,
+    "Cân nặng tối đa", even,
+    "Cân nặng tối thiểu", even * 8 / 10);
